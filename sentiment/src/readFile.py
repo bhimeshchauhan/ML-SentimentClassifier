815f8d920d522e290acd90aa9220f4f3385a44a0
bhimeshchauhan <bhimeshchauhan@gmail.com>
2018-05-26 22:40:11 -0400

    a rather easier way to remove all the charachters that are not alphabets

--- sentiment/src/readFile.py
+++ sentiment/src/readFile.py
@@ -1,40 +1,28 @@
-import nltk
+import re
 
 class readFile(object):
 
     def read(self, file):
-
         """
         Reads txt files passed to the module as file path from the source directory
         :return: List of text broken at '\n' character.
         """
-
         try:
             with open(file, 'rU') as f:
                 fList = f.readlines()
-            self.debug(fList)
+            self.noiseremoval(self, fList)
         except Exception as ex:
             print(ex)
 
-
-
-    def debug(self, fileItems):
+    @staticmethod
+    def noiseremoval(self, fileItems):
         """
         For debug to print the statements from the file.
         :return: Items from the read list.
         """
-        nltk.download('words')
-        english_vocab = set(w.lower() for w in nltk.corpus.words.words())
         for item in fileItems:
-            sList = item.strip().split()
-            print("old slist ", sList)
-            for words in sList:
-                if words not in english_vocab:
-                    sList.remove(words)
-            print(sList)
-        print(',' in english_vocab)
-
-
+            print("old item  :", item)
+            print(re.sub(r'[^a-zA-Z ]', '', str(item)).split())
 
 
 if __name__ == "__main__":
